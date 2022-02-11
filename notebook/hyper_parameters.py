06e0b69efe4781d8915b2cddecd22a9a856b7e18
Bryon Baker <brbaker@redhat.com>
2022-02-11 04:17:44 +0000

    Added debug info to the paramaters to helkp instructor's debug student config issues

--- notebook/hyper_parameters.py
+++ notebook/hyper_parameters.py
@@ -1,9 +1,27 @@
 def get_hyper_paras():
+    # ##########################################################################
+    #
+    # PLACE YOUR STUDENT CONFIGURATION INFORMATION IN THIS SECTION OF THE CODE
+    #
     user_id = "opentlc-mgr"
-    PROJECT_NAME = "CustomerChurn-opentlc-mgr"
-    EXPERIMENT_NAME = "CustomerChurn-opentlc-mgr"
-    experiment_name = "customerchurn-opentlc-mgr"
-    minioFilename="part-00000-aa888b5b-fd89-431e-831f-cfd9d79e4548-c000.csv"
-    s3BucketFullPath = "full_data_csv"+user_id+"/"+minioFilename
+    minioFilename = "part-00000-aa888b5b-fd89-431e-831f-cfd9d79e4548-c000.csv"
+    # ##########################################################################
     
-    return user_id,PROJECT_NAME,EXPERIMENT_NAME,experiment_name,s3BucketFullPath
+    # ##########################################################################    
+    #
+    # DO NOT CHANGE ANY CODE BELOW THIS LINE
+    #
+    # ##########################################################################
+    PROJECT_NAME = "CustomerChurn-"+user_id
+    EXPERIMENT_NAME = PROJECT_NAME
+    experiment_name = EXPERIMENT_NAME.lower()
+    s3BucketFullPath = "full_data_csv"+user_id+"/"+minioFilename
+
+    print("STUDENT CONFIGURATION")
+    print("=====================")
+    print(f"User ID: \"{user_id}\"")
+    print(f"Project name: \"{PROJECT_NAME}\"")
+    print(f"Experiment name: \"{EXPERIMENT_NAME}\", \"{experiment_name}\"")
+    print(f"S3 Bucket full storage path: \"{s3BucketFullPath}\"")
+
+    return user_id, PROJECT_NAME, EXPERIMENT_NAME, experiment_name, s3BucketFullPath
