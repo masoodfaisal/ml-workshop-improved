b1a9644a2f03962fbbd19dd082640c4a5765445b
Bryon Baker <brbaker@redhat.com>
2022-02-11 03:01:26 +0000

    Added S3 config parm

--- notebook/hyper_parameters.py
+++ notebook/hyper_parameters.py
@@ -1,7 +1,9 @@
 def get_hyper_paras():
-    user_id = "user29"
-    PROJECT_NAME = "CustomerChurnUser60"
-    EXPERIMENT_NAME = "CustomerChurnUser60"
-    experiment_name = "customerchurnuser29"
-    return user_id,PROJECT_NAME,EXPERIMENT_NAME,experiment_name
+    user_id = "opentlc-mgr"
+    PROJECT_NAME = "CustomerChurn-opentlc-mgr"
+    EXPERIMENT_NAME = "CustomerChurn-opentlc-mgr"
+    experiment_name = "customerchurn-opentlc-mgr"
+    minioFilename="part-00000-aa888b5b-fd89-431e-831f-cfd9d79e4548-c000.csv"
+    s3BucketFullPath = "full_data_csv"+user_id+"/"+minioFilename
     
+    return user_id,PROJECT_NAME,EXPERIMENT_NAME,experiment_name,s3BucketFullPath
